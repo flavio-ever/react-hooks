9a533f4d46f77388744cae8859d27c141caa64df
Flavio Ever <flavio.ever@icloud.com>
2019-12-21 15:09:25 -0300

    Init 🚀

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
+    'prettier/react'
+  ],
+  parser: 'babel-eslint',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+  ],
+  rules: {
+    // Apontar todas as regras que não encontrar como um erro
+    'prettier/prettier': 'error',
+    // JSX
+    'react/jsx-filename-extension': [
+      // Warn somente se extencoes não forem jsx quanto js
+      'warn',
+      { extensions : ['.jsx', '.js'] }
+    ],
+    'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'no-console': ['error', { allow: ['tron']}],
+    'no-param-reassign': 'off'
+  },
+};
---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div className="App">Olá, ReactJS</div>;
+}
+
+export default App;
